b58348546d4f83917aeae4c604c847d0806fe0e5
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-01-23 07:02:06 +0700

    update view home

--- resources/views/components/index.blade.php
+++ resources/views/components/index.blade.php
@@ -1,19 +1,20 @@
 <!doctype html>
 
-<html>
+<html class="scroll-smooth">
     <head>
         <title>Cek Cabai App @yield('title')</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
         @vite('resources/css/app.css')
     </head>
     
     <body class="min-h-screen">
         <x-navbar />
  
-        <div class="container min-h-screen min-w-full">
+        <main class="container min-h-screen min-w-full">
             @yield('content')
-        </div>
+        </main>
 
         <x-footer />
     </body>
